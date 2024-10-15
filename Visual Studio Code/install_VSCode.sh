0ccd670db00a8cb874417b7741c4a68d5279f124
gabriel barbato <gabrielbsp12@gmail.com>
2024-10-15 16:14:39 -0300

    Update install_VSCode.sh

--- Visual Studio Code/install_VSCode.sh
+++ Visual Studio Code/install_VSCode.sh
@@ -3,27 +3,32 @@
 vscode_url='https://code.visualstudio.com/sha/download?build=stable&os=darwin-universal'
 vscode_ZIP_PATH="/tmp/VSCode-darwin-universal.zip"
 
-# verify if VSCode is already installed
+# check if VS Code is already installed
 if [ -d "/Applications/Visual Studio Code.app" ]; then
   echo "O VS Code já está instalado."
   exit 1
 fi
 
-curl -L -o -s "$vscode_ZIP_PATH" "$vscode_url"
+# download the zip file of VS Code
+curl -L -s -o "$vscode_ZIP_PATH" "$vscode_url"
 
-unzip $vscode_ZIP_PATH -d /tmp
+# unzip the zip file in /tmp
+unzip -q "$vscode_ZIP_PATH" -d /tmp
 
+# move app to Aplicativos
 mv /tmp/Visual\ Studio\ Code.app /Applications/
 
+# remove zip file
 rm -rf "$vscode_ZIP_PATH"
 
 echo "Aguarde uns instantes..."
 sleep 15
 
+# check if the installation was sucessfull
 if [ -d "/Applications/Visual Studio Code.app" ]; then
     echo "O VS Code foi instalado com sucesso!"
     exit 0
 else
-    echo "O VS Code não pode ser atualizado."
+    echo "O VS Code não pôde ser instalado."
     exit 1
 fi
